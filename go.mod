b7e025624f45d5a82f17c17dbba7fc48ef5dfe17
lptrk <lktpriv@gmail.com>
2024-11-29 11:13:20 +0100

    init

--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module properties-yamlized
+
+go 1.23.1
+
+require gopkg.in/yaml.v3 v3.0.1
--- main.go
+++ main.go
@@ -1,7 +1,105 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
 
 func main() {
-	fmt.Println("Ready to Go")
+	inputFile := "assets/test_config.properties"
+	outputFile := "test_config.yml"
+
+	properties, err := readProperties(inputFile)
+	if err != nil {
+		fmt.Printf("Fehler beim Lesen der Datei: %v\n", err)
+		return
+	}
+
+	nestedMap := createNestedMap(properties)
+
+	if err := writeYAMLWithSpaces(nestedMap, outputFile); err != nil {
+		fmt.Printf("Fehler beim Schreiben der YAML-Datei: %v\n", err)
+		return
+	}
+
+	fmt.Printf("Konvertierung abgeschlossen: %s -> %s\n", inputFile, outputFile)
+}
+
+func readProperties(filename string) (map[string]string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	properties := make(map[string]string)
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+
+		if len(line) == 0 || strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) != 2 {
+			continue
+		}
+
+		key := strings.TrimSpace(parts[0])
+		value := strings.TrimSpace(parts[1])
+		properties[key] = value
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return properties, nil
+}
+
+func createNestedMap(flatMap map[string]string) map[string]interface{} {
+	nestedMap := make(map[string]interface{})
+
+	for key, value := range flatMap {
+		parts := strings.Split(key, ".")
+		currentMap := nestedMap
+
+		for i, part := range parts {
+			if i == len(parts)-1 {
+				currentMap[part] = value
+			} else {
+				if _, exists := currentMap[part]; !exists {
+					currentMap[part] = make(map[string]interface{})
+				}
+				currentMap = currentMap[part].(map[string]interface{})
+			}
+		}
+	}
+
+	return nestedMap
+}
+
+func writeYAMLWithSpaces(data map[string]interface{}, filename string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	encoder := yaml.NewEncoder(file)
+	defer encoder.Close()
+
+	encoder.SetIndent(2)
+
+	if err := encoder.Encode(data); err != nil {
+		return err
+	}
+
+	return nil
 }
